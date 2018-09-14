3c01518888e7facb7514e9d5fa25a408eb54fc5a
nikeshthapa255 <36557452+nikeshthapa255@users.noreply.github.com>
2018-09-14 23:48:00 +0530

    Update secondlarge.py

--- secondlarge.py
+++ secondlarge.py
@@ -1,6 +1,5 @@
-def give(a):
-    return (a if len(a)<4 else give([max(a[i*2],a[i*2+1]) for i in range(int(len(a)/2))]))
-
+def give(a):return (a if len(a)<4 else give([max(a[i*2],a[i*2+1]) for i in range(int(len(a)/2))]))
+import random
 a=list(map(int,input().split()))
-print(sorted(give(a))[-2])
-
+ans=[sorted(give(random.sample(a,len(a))))[-2] for i in range(1000)]
+print(max(set(ans),key=ans.count))
